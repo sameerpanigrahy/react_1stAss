6fd2c69dbf89482dedb5b42a368664ef3990a322
sameerpanigrahi <panigrahisameer2000@gmail.com>
2023-01-17 22:33:49 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Search from './Search';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="home__body">
+        <img
+          src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+          alt="logo"
+        />
+        <div className="home__inputContainer">
+          <Search />
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
